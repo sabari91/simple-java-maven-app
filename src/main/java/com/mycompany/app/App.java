36a472756aa7935710c3eabe77b724c34c460ded
sabari91 <sabarin965@gmail.com>
2018-08-12 14:54:01 +0800

    Commit second

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!, test commit";
+    private final String message = "Hello World!, test commit final time";
 
     public App() {}
 
